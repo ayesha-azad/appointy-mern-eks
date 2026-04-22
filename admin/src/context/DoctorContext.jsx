d5c597e20248bbed6b4beb24e9e21ef0a1ce22f1
Ayesha Azad <ayeshaazad@Ayeshas-MacBook-Pro.local>
2026-04-22 15:43:08 +0500

    Fixing 405 error

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -5,7 +5,7 @@ import { toast } from "react-toastify";
 export const DoctorContext = createContext();
 
 const DoctorContextProvider = (props) => {
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = (import.meta.env.VITE_BACKEND_URL || "").trim();
 
   const [dToken, setDToken] = useState(
     localStorage.getItem("dToken") || ""
